18b74679b21b0c316d3862eabf8112930935a67d
Paul Bakeman <100813108+pandmbake@users.noreply.github.com>
2022-06-25 15:56:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function spinCoin (event) {
+    document.getElementById("spin").style.display = 'flex';
+    document.getElementById("heads").style.display = 'none';
+    document.getElementById("tails").style.display = 'none';
+    document.getElementById("button").style.display = 'none';
+    setTimeout(flip, 2000);
+}
 
+
+function flip (event) {
+    let randomSide = Math.floor(Math.random() * 2);
+      if (randomSide == 1) {
+        document.getElementById("heads").style.display = 'flex';
+        document.getElementById("tails").style.display = 'none';
+        document.getElementById("spin").style.display = '';
+        document.getElementById("button").style.display = 'flex';
+        
+      } else {
+        document.getElementById("tails").style.display = 'flex';
+        document.getElementById('heads').style.display = 'none';
+        document.getElementById("spin").style.display = '';
+        document.getElementById("button").style.display = 'flex';
+        
+      }
+
+
+      
+    }
